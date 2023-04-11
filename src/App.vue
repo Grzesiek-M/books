5899706704b6384650438afd15c5ff0d421c2bf2
Grzesiek-W <grzesiek@zwarszawy.pl>
2023-04-11 12:00:09 +0200

    Init_commit

---
+++ src/App.vue
@@ -0,0 +1,70 @@
+<template>
+  <div id="app">
+
+    <!-- heading -->
+    <header>
+      <h1>Books<span>.app</span></h1>
+    </header>
+    <ul>
+  <li
+    :key="index"
+    v-for="(book, index) in books">
+    {{ book.title }}, {{ book.price }}
+    <button @click="removeBook(index)">Remove</button>
+  </li>
+</ul>
+    <p v-show="!books.length">No books...</p>
+    <p v-if="books.length >5" >{{books.length}} books! </p>
+    <p v-else-if="books.length > 1">Not too many of them… </p>
+    <p v-else-if="books.length === 1">One single book! </p>
+    <p v-else>Go get some books!</p>
+    <!-- add book form -->
+  <form @submit.prevent="handleSubmit">
+  <label>
+    Title:
+    <input v-model="form.title" type="text" name="title" placeholder="title">
+    {{ form.title }}
+  </label>
+  <label>
+    price:
+    <input v-model="form.price" type="number" name="price">
+    {{ form.price }}
+  </label>
+  <button>Add book</button>
+  </form>
+
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App',
+  data: () => ({
+    books: [
+      {
+        title: 'The Catcher in the Rye',
+        price: 20
+      },
+      {
+        title: 'Of Mice and Men',
+        price: 18
+      }
+    ],
+    form: {
+      title: '',
+      price: 0
+    }
+
+  }),
+  methods: {
+    removeBook (index) {
+      this.books.splice(index, 1)
+    },
+    handleSubmit () {
+      this.books.push({ ...this.form })
+      this.form.price = 0
+      this.form.title = ''
+    }
+  }
+}
+</script>
